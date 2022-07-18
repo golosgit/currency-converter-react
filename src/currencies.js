3d33c495a3c3cc22908a27280fd9a5e8242b8672
Grzegorz Gołos <golosgit@gmail.com>
2022-07-18 17:28:49 +0200

    added separate file for currencies

---
+++ src/currencies.js
@@ -0,0 +1,24 @@
+const currencies = [
+  {
+    name: "polski złoty",
+    shortName: "PLN",
+    rate: 1,
+  },
+  {
+    name: "euro",
+    shortName: "EUR",
+    rate: 4.6313,
+  },
+  {
+    name: "dolar amerykański",
+    shortName: "USD",
+    rate: 4.4209,
+  },
+  {
+    name: "korona czeska",
+    shortName: "CZK",
+    rate: 0.1874,
+  },
+];
+
+export default currencies;
