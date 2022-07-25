31714eb4811f47d8736e1aaeaec52f2b013d2ae0
Grzegorz Gołos <golosgit@gmail.com>
2022-07-25 19:34:08 +0200

    code refactor - changed Form element and moved code to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,81 @@
+import { useState } from "react";
 import Form from "./Form";
 import DateTime from "./DateTime";
 import Container from "./Container";
+import currencies from "./currencies";
 
 function App() {
+  const [exchangeAmount, setExchangeAmount] = useState(0);
+  const [exchangeCurrency, setExchangeCurrency] = useState("PLN");
+  const [resultCurrency, setResultCurrency] = useState("EUR");
+
+  const exchangeRate =
+    currencies.find(({ shortName }) => shortName === exchangeCurrency).rate /
+    currencies.find(({ shortName }) => shortName === resultCurrency).rate;
+
+  const result = exchangeAmount * exchangeRate;
+
+  const onInputChange = ({ target }) => {
+    if (Math.sign(target.value) < 0) {
+      setExchangeAmount(target.value.slice(1));
+    } else {
+      setExchangeAmount(target.value);
+    }
+  };
+  const onExchangeCurrencyChange = ({ target }) => setExchangeCurrency(target.value);
+  const onResultCurrencyChange = ({ target }) => setResultCurrency(target.value);
+
   return (
     <Container>
-      <Form date={<DateTime />} />
+      <Form>
+        <legend className="form__legend">Kalkulator walut</legend>
+        <DateTime />
+        <p>
+          <label className="form__label form--alignRight" htmlFor="exchange">
+            Kwota do wymiany:
+          </label>
+          <input
+            value={exchangeAmount}
+            onChange={onInputChange}
+            className="form__input form--alignRight"
+            type="number"
+            min="0"
+            step="1"
+            id="exchange"
+          />
+          <select value={exchangeCurrency} onChange={onExchangeCurrencyChange} className="form__select">
+            {currencies.map(({ shortName, name }) => (
+              <option value={shortName} key={shortName}>
+                {shortName} ({name})
+              </option>
+            ))}
+          </select>
+        </p>
+        <p>
+          <label className="form__label form--alignRight" htmlFor="receive">
+            Otrzymasz:
+          </label>
+          <input
+            className="form__input form--alignRight form__input--readonly"
+            type="number"
+            value={result.toFixed(2)}
+            id="receive"
+            readOnly
+          />
+          <select value={resultCurrency} onChange={onResultCurrencyChange} className="form__select">
+            {currencies.map(({ shortName, name }) => (
+              <option value={shortName} key={shortName}>
+                {shortName} ({name})
+              </option>
+            ))}
+          </select>
+        </p>
+        <p className="form__exchangeInfo">
+          1 {exchangeCurrency} to {exchangeRate.toFixed(5)} {resultCurrency}
+        </p>
+      </Form>
     </Container>
-  )
+  );
 }
 
 export default App;
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,76 +1,10 @@
-import { useState } from "react";
 import "./style.css";
-import currencies from "./currencies";
 
-const Form = ({ date }) => {
-  const [exchangeAmount, setExchangeAmount] = useState(0);
-  const [exchangeCurrency, setExchangeCurrency] = useState("PLN");
-  const [resultCurrency, setResultCurrency] = useState("EUR");
-
-  const exchangeRate =
-    currencies.find(({ shortName }) => shortName === exchangeCurrency).rate /
-    currencies.find(({ shortName }) => shortName === resultCurrency).rate;
-
-  const result = exchangeAmount * exchangeRate;
-
-  const onInputChange = ({ target }) => {
-    if (Math.sign(target.value) < 0) {
-      setExchangeAmount(target.value.slice(1));
-    } else {
-      setExchangeAmount(target.value);
-    }
-  };
-  const onExchangeCurrencyChange = ({ target }) => setExchangeCurrency(target.value);
-  const onResultCurrencyChange = ({ target }) => setResultCurrency(target.value);
-
+const Form = ({ children }) => {
   return (
     <form className="form">
       <fieldset className="form__fieldset">
-        <legend className="form__legend">Kalkulator walut</legend>
-        {date}
-        <p>
-          <label className="form__label form--alignRight" htmlFor="exchange">
-            Kwota do wymiany:
-          </label>
-          <input
-            value={exchangeAmount}
-            onChange={onInputChange}
-            className="form__input form--alignRight"
-            type="number"
-            min="0"
-            step="1"
-            id="exchange"
-          />
-          <select value={exchangeCurrency} onChange={onExchangeCurrencyChange} className="form__select">
-            {currencies.map(({ shortName, name }) => (
-              <option value={shortName} key={shortName}>
-                {shortName} ({name})
-              </option>
-            ))}
-          </select>
-        </p>
-        <p>
-          <label className="form__label form--alignRight" htmlFor="receive">
-            Otrzymasz:
-          </label>
-          <input
-            className="form__input form--alignRight form__input--readonly"
-            type="number"
-            value={result.toFixed(2)}
-            id="receive"
-            readOnly
-          />
-          <select value={resultCurrency} onChange={onResultCurrencyChange} className="form__select">
-            {currencies.map(({ shortName, name }) => (
-              <option value={shortName} key={shortName}>
-                {shortName} ({name})
-              </option>
-            ))}
-          </select>
-        </p>
-        <p className="form__exchangeInfo">
-          1 {exchangeCurrency} to {exchangeRate.toFixed(5)} {resultCurrency}
-        </p>
+        {children}
       </fieldset>
     </form>
   );
